7294d7688f5f441287357101cc6383dad764e6c8
sdirnboeck <sdirnboeck@gmail.com>
2022-05-10 12:38:10 +0200

    selina update

--- 07Ue-2022-05-10/UE07.py
+++ 07Ue-2022-05-10/UE07.py
@@ -54,21 +54,49 @@ pos = [r]
 
 for t in range(int(1 / dt * 365 * 2)):
     k1 = acc(r)
-    k2 = acc(r + dt / 2 * k1)
-    k3 = acc(r + dt / 2 * k2)
-    k4 = acc(r +     dt * k3)
+    k1_v = v 
+
+    k2 = acc(r + dt / 2 * k1_v)
+    k2_v = v + dt/2*k1
+
+    k3 = acc(r + dt / 2 * k2_v)
+    k3_v = v + dt/2*k2
+
+    k4 = acc(r +     dt * k3_v)
+    k4_v = v + dt*k3
+
     phi = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+    phi_v = 1/6 * (k1_v + 2*k2_v + 2*k3_v + k4_v)
 
     v_ = v
     v = v + dt * phi
     
-    r = r + dt * v_
+    r = r + dt * phi_v
     pos.append(r)
 
 pos = np.array(pos)
 
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Earth Orbit integrated with dt = '+str(dt))
+plt.title('Earth Orbit integrated with dt = '+str(dt)+' using RK4')
+plt.plot(pos[:, 0], pos[:, 1])
+plt.show()
+
+
+#semi-implizierter Euler
+v = np.array([0, -0.017326])
+r = np.array([1, 0])
+dt = 1
+pos = [r]
+
+for t in range(int(1 / dt * 365 *2)):
+    v = v + dt * acc(r)
+    r = r + dt * v
+    pos.append(r)
+pos = np.array(pos)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Earth Orbit integrated with dt = '+str(dt)+' using semi-implicit Euler')
 plt.plot(pos[:, 0], pos[:, 1])
 plt.show()

c71f2abae47dc0787c5677a9adb71beb8f9edfff
Fabian Helmberger <fabi.helmi1999@gmx.at>
2022-05-15 22:47:00 +0200

    Formal stuff

--- 07Ue-2022-05-10/UE07.py
+++ 07Ue-2022-05-10/UE07.py
@@ -1,3 +1,5 @@
+from cProfile import label
+from cmath import exp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,23 +24,34 @@ def acc(r):
     out = - (G * m_s / norm**3 ) * r
     return out
     
-v = np.array([0, -0.017326])
-r = np.array([1, 0])
-dt = 1
-pos = [r]
 
+def exp_Eu(r0, v0, dt, T):
+    r = r0
+    v = v0
+    pos = [r0]
+    for t in range(int(1 / dt * T)):
+        v_ = v
+        v = v + dt * acc(r)
+        r = r + dt * v_
+        pos.append(r)
+    pos = np.array(pos)
+    return pos
 
-for t in range(int(1 / dt * 365 *2)):
-    v_ = v
-    v = v + dt * acc(r)
-    r = r + dt * v_
-    pos.append(r)
-pos = np.array(pos)
+r0 = np.array([1, 0])
+v0 = np.array([0, -2*np.pi / 365])
+dt1 = 1
+dt2 = 0.01
+T = 365
+pos1 = exp_Eu(r0, v0, dt1, T)
+pos2 = exp_Eu(r0, v0, dt2, T)
+
 
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Earth Orbit integrated with dt = '+str(dt))
-plt.plot(pos[:, 0], pos[:, 1])
+plt.title('Earth Orbit integrated using the explicit euler method')
+plt.plot(pos1[:, 0], pos1[:, 1], label="dt = "+str(dt1)+' days')
+plt.plot(pos2[:, 0], pos2[:, 1], label="dt = "+str(dt2)+' days')
+plt.legend()
 plt.show()
 
 
@@ -46,29 +59,71 @@ plt.show()
 
 
 
-v = np.array([0, -0.017326])
-r = np.array([1, 0])
-dt = 1
-pos = [r]
+def RK4(r0, v0, dt, T):
+    r = r0
+    v = v0
+    pos = [r0]
+    for t in range(int(1 / dt * T)):
+        k1 = acc(r)
+        k2 = acc(r + dt / 2 * k1)
+        k3 = acc(r + dt / 2 * k2)
+        k4 = acc(r +     dt * k3)
+        phi = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
 
+        v_ = v
+        v = v + dt * phi
+        
+        r = r + dt * v_
+        pos.append(r)
+    pos = np.array(pos)
+    return pos
 
-for t in range(int(1 / dt * 365 * 2)):
-    k1 = acc(r)
-    k2 = acc(r + dt / 2 * k1)
-    k3 = acc(r + dt / 2 * k2)
-    k4 = acc(r +     dt * k3)
-    phi = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+v0 = np.array([0, -0.017326])
+r0 = np.array([1, 0])
 
-    v_ = v
-    v = v + dt * phi
-    
-    r = r + dt * v_
-    pos.append(r)
-
-pos = np.array(pos)
+pos1 = RK4(r0, v0, dt1, T)
+pos2 = RK4(r0, v0, dt2, T)
 
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Earth Orbit integrated with dt = '+str(dt))
-plt.plot(pos[:, 0], pos[:, 1])
+plt.title('Earth Orbit integrated using the Runge-Kutta method')
+plt.plot(pos1[:, 0], pos1[:, 1], label="dt = "+str(dt1)+' days')
+plt.plot(pos2[:, 0], pos2[:, 1], label="dt = "+str(dt2)+' days')
+plt.legend()
 plt.show()
+
+
+
+# Transfer
+# We want to find a velocity, such that the maximum radial distance is R_m = 1.5
+
+R_m = 1.5
+
+
+r = np.array([1, 0])
+v = np.array([0, -2*np.pi / 365])
+dt = 1
+dv = 1 / 365
+pos = [r]
+
+r_max = 0
+
+while True:
+    ###########################################
+    # This should be done by calling a function
+    for t in range(int(1 / dt * 365 * 2)):
+        k1 = acc(r)
+        k2 = acc(r + dt / 2 * k1)
+        k3 = acc(r + dt / 2 * k2)
+        k4 = acc(r +     dt * k3)
+        phi = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+        v_ = v
+        v = v + dt * phi
+        
+        r = r + dt * v_
+        pos.append(r)
+        print(r)
+    pos = np.array(pos)
+    ###########################################
+    

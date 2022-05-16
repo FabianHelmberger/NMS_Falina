311ef553a452b6e0e096d47e64f2d38c7c7b4554
Fabian Helmberger <fabi.helmi1999@gmx.at>
2022-05-16 23:12:57 +0200

    Transfer finished

--- 07Ue-2022-05-10/UE07.py
+++ 07Ue-2022-05-10/UE07.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
+# Units
 
 # SI units
 m_s = 1.989 * 1e30
@@ -16,16 +17,17 @@ G = G * 1 / r_e**3 * m_e * day **2
 m_s = m_s / m_e
 m_e = 1
 r_e = 1
-print(G*m_s)
 
 
-# Explizites Euler Verfahren
+# Physics
 
 def acc(r):
     norm = np.linalg.norm(r)
     out = - (G * m_s / norm**3 ) * r
     return out
-    
+
+
+# Explizites Euler Verfahren
 
 def exp_Eu(r0, v0, dt, T):
     r = r0
@@ -48,9 +50,9 @@ v0 = np.array([0, -2*np.pi / 365])
 dt1 = 1
 dt2 = 0.01
 T = 365
+
 pos1 = exp_Eu(r0, v0, dt1, T)[0]
 pos2 = exp_Eu(r0, v0, dt2, T)[0]
-
 
 plt.xlabel('x')
 plt.ylabel('y')
@@ -62,8 +64,6 @@ plt.show()
 
 
 # Runge-Kutta
-
-
 
 def RK4(r0, v0, dt, T):
     r = r0
@@ -82,15 +82,13 @@ def RK4(r0, v0, dt, T):
         k4_v = v + dt*k3
         phi = 1/6 * (k1 + 2*k2 + 2*k3 + k4)
         phi_v = 1/6 * (k1_v + 2*k2_v + 2*k3_v + k4_v)
-
-        
         
         v = v + dt * phi
         r = r + dt * phi_v
 
-
         pos.append(r)
         vel.append(v)
+
     pos = np.array(pos)
     vel = np.array(vel)
     return pos, vel
@@ -105,6 +103,37 @@ plt.plot(pos1[:, 0], pos1[:, 1], label="dt = "+str(dt1)+' days')
 plt.plot(pos2[:, 0], pos2[:, 1], label="dt = "+str(dt2)+' days')
 plt.legend()
 plt.show()
+
+
+#semi-implizierter Euler
+
+def SIM_Eu(r0, v0, dt, T):
+    r = r0
+    v = v0
+    pos = [r0]
+    vel = [v0]
+    for t in range(int(1 / dt * T)):
+        v = v + dt * acc(r)
+        r = r + dt * v
+        pos.append(r)
+        vel.append(v)
+    pos = np.array(pos)
+    vel = np.array(vel)
+    return pos, vel
+
+v0 = np.array([0, -0.017326])
+r0 = np.array([1, 0])
+dt = 1
+T = 365
+
+pos = SIM_Eu(r0, v0, dt, T)[0]
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Earth Orbit integrated with dt = '+str(dt)+' using semi-implicit Euler')
+plt.plot(pos[:, 0], pos[:, 1])
+plt.show()
+
 
 #impliziertes Eulerverfahren 
 
@@ -127,8 +156,10 @@ v0 = np.array([0, -2*np.pi / 365])
 dt1 = 1
 dt2 = 0.01
 T = 365
+
 pos1 = imp_Eu(r0, v0, dt1, T)[0]
 pos2 = imp_Eu(r0, v0, dt2, T)[0]
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Earth Orbit integrated using the implicit euler method')
@@ -139,6 +170,11 @@ plt.show()
 
 
 # Energien 
+
+def E(pos, vel):
+    E = m_e*vel/2 - G*m_e*m_s/np.abs(pos)
+    return E
+
 dt = 0.01
 r0 = np.array([1, 0])
 v0 = np.array([0, -2*np.pi / 365])
@@ -153,9 +189,6 @@ vel_expE = exp_Eu(r0, v0, dt2, T)[1]
 pos_RK4 = RK4(r0, v0, dt1, T)[0]
 vel_RK4 = RK4(r0, v0, dt2, T)[1]
 
-def E(pos, vel):
-    E = m_e*vel/2 - G*m_e*m_s/np.abs(pos)
-    return E
 
 E_impE=E(pos_impE, vel_impE)
 E_expE=E(pos_expE, vel_expE)
@@ -170,19 +203,8 @@ plt.ylabel('E')
 plt.title('Energies')
 plt.plot(E_impE, label='implicit Euler')
 plt.plot(E_expE, label='explicit Euler')
-#plt.plot(E_RK4, label='RK4')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
 
 
 # Transfer
@@ -194,49 +216,26 @@ R_mars = 1.5
 
 r0 = np.array([1, 0])
 v0 = np.array([0, -2*np.pi / 365])
-dt = 0.01
-dv = v0[1] / 1000
+dt1 = 0.1
+dv = v0[1] / 100
 
-pos = RK4(r0, v0, dt1, 2*T)
+pos = RK4(r0, v0, dt1, 2*T)[0]
 r_max = np.max(np.abs(pos))
 radii = []
 
 while r_max < R_mars:
     v0[1] = v0[1] + dv
-    pos = RK4(r0, v0, dt1, 2*T)
+    pos = RK4(r0, v0, dt1, 2*T)[0]
     r_max = np.max(np.abs(pos))
-    print(r_max, v0)
-
+    #print(r_max, v0)
     radii.append([v0[1], r_max])
+
 radii = np.array(radii)
 
 fit = interp1d(radii[:, 1], radii[:, 0], kind='cubic')
 V_mars = fit(R_mars)
-pos_mars = RK4(r0, np.array([0, V_mars]), dt1, 2*T)
 
-t_max = np.where(pos_mars == np.amax(pos_mars))
-print('', t_max[0] * dt1)
-print(pos_mars)
+pos_mars = RK4(r0, np.array([0, V_mars]), dt1, 2*T)[0]
+t_max = np.where(np.abs(pos_mars) == np.max(np.abs(pos_mars)))
 
-plt.title('Earth Orbit integrated with dt = '+str(dt)+' using RK4')
-plt.plot(pos_mars[:, 0], pos_mars[:, 1])
-plt.show()
-
-
-#semi-implizierter Euler
-v = np.array([0, -0.017326])
-r = np.array([1, 0])
-dt = 1
-pos = [r]
-
-for t in range(int(1 / dt * 365 *2)):
-    v = v + dt * acc(r)
-    r = r + dt * v
-    pos.append(r)
-pos = np.array(pos)
-
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Earth Orbit integrated with dt = '+str(dt)+' using semi-implicit Euler')
-plt.plot(pos[:, 0], pos[:, 1])
-plt.show()
+print('Tage bis zum Mars: ', t_max[0] * dt1)

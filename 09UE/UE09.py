9301cfb3ee20821eee4b943ae804af120b7a142f
sdirnboeck <sdirnboeck@gmail.com>
2022-05-25 17:26:52 +0200

    voller Code aber noch Fehler

--- 09UE/UE09.py
+++ 09UE/UE09.py
@@ -63,37 +63,41 @@ grid = rand_init(L)
 mag_T = np.zeros( len(Temps) )
 mag_square_T = np.zeros( len(Temps) )
 mag_fourth_T = np.zeros( len(Temps) )
+size = np.array([4,8])
+
+for ind in range(len(size)):
+    N_warm_up = size[ind]
+    N_mess = size[ind]
+
+    for temperatures in range(len(Temps)):
+        Temp = Temps[temperatures]
+        mag = 0 
+        mag_square = 0 
+        mag_fourth = 0 
+
+        # warm up sweeps
+        for n in range(N_warm_up):
+            grid = sweep(grid, Anzahl_Gitterplätze, Temp)
+        
+        # messungen
+        for n in range(N_mess):
+            grid = sweep(grid, Anzahl_Gitterplätze, Temp)
+            mag = mag + np.sum(grid)/(L*L)
+            mag_square = mag_square + (np.sum(grid)/(L*L))**2 
+            mag_fourth = mag_fourth + (np.sum(grid)/(L*L))**4
+
+        mag_T[temperatures] = mag/N_mess
+        mag_square_T[temperatures] = mag_square/N_mess
+        mag_fourth_T[temperatures] = mag_fourth/N_mess
+
+    pl.plot(mag_T)
+    pl.show()
 
 
-for temperatures in range(len(Temps)):
-    Temp = Temps[temperatures]
-    mag = 0 
-    mag_square = 0 
-    mag_fourth = 0 
-
-    # warm up sweeps
-    for n in range(N_warm_up):
-        grid = sweep(grid, Anzahl_Gitterplätze, Temp)
-    
-    # messungen
-    for n in range(N_mess):
-        grid = sweep(grid, Anzahl_Gitterplätze, Temp)
-        mag = mag + np.sum(grid)/(L*L)
-        mag_square = mag_square + (np.sum(grid)/(L*L))**2 
-        mag_fourth = mag_fourth + (np.sum(grid)/(L*L))**4
-
-    mag_T[temperatures] = mag/N_mess
-    mag_square_T[temperatures] = mag_square/N_mess
-    mag_fourth_T[temperatures] = mag_fourth/N_mess
-
-
-    # - über Temperatur loopen um Listen von Messgrößen berechnen 
-    # - das ganze über verschiedene N loopen
+   
+ 
 
  
-pl.plot(mag_T)
-pl.show()
-
 
 
 

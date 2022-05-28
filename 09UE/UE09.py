34c3a795b4c3c5850abd9a45d469511a37cda6ec
Fabian Helmberger <fabi.helmi1999@gmx.at>
2022-05-28 19:25:25 +0200

    minor changes

--- 09UE/UE09.py
+++ 09UE/UE09.py
@@ -2,58 +2,62 @@ from re import T
 import numpy as np
 from scipy.constants import k
 import matplotlib.pyplot as pl
+import copy 
 
+k = 1
+Relative_neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
 # initialise random lattice
 def rand_init(L):
     grid = (np.random.randint(2, size = (L,L))-0.5)*2
     return grid
 
+def flip(grid, x, y):
+    grid_ = copy.deepcopy(grid)
+    grid_[x,y] = -1*grid[x,y]
+    return grid_
 
-def  energy (i,j, grid, spin_current):
-    dim = np.shape(grid)[0]
+def energy (x, y, grid, j):
+    dim_x = np.shape(grid)[0]
+    dim_y = np.shape(grid)[1]
     energy = 0
-    
+    x0, y0 = x, y
+    spin = grid[x0, y0]
 
-    for ind in range(len(Inn)):
-        i = i + Inn[ind]
-        j = j + Jinn[ind]
-
-        neighbour_value = grid[ (i+dim)%dim ][ (j+dim)%dim ] 
-        if neighbour_value != spin_current: 
+    for coord in Relative_neighbours:
+        x, y = x0 + coord[0], y0 + coord[1]
+        equal_spin = (spin == grid[(dim_x + x)%dim_x][(dim_y + y)%dim_y])
+        if equal_spin   : 
             energy = energy - 1 
-        else:
+        if not equal_spin:
             energy = energy + 1 
-    return J*energy
+    return j*energy
 
-def sweep(grid, N, Temp):
+def sweep(grid, N, temp):
+    sz = np.shape(grid)[0]
+
     for n in range(N):
-        i = round(random.random()*(L-1))
-        j = round(random.random()*(L-1))
-        spin_current = grid[i][j]
-
-        E_current = energy(i, j, grid, spin_current)
-        E_flipped = energy(i, j, grid, spin_current*(-1))
-
-        
+        rand = np.random.randint(sz, size=2)
+        x, y = rand[0], rand[1]
+        E_current = energy(x, y, grid, 1              )
+        E_flipped = energy(x, y, flip(grid, x, y),1   )
         delta_E = E_flipped - E_current
-        r = np.exp(-delta_E/(k_b*Temp))
+        r = np.exp(-delta_E/(k*temp))
 
-        if random.random() < min(1,r): 
-            grid[i][j] = (-1) * grid[i][j]
-    
+        if np.random.random() < min(1,r): 
+            grid[x][y] = (-1) * grid[x][y]
+            grid = flip(grid, x, y)
+        #print(grid)
     return grid
-
 
 #main
 random = np.random.default_rng()
 
-
 # Parameters than can be changed
-
-L = 20              # Lattice size 
-size = np.array([4,8])
-Temps = [1,2,3,4]
+Sizes   = np.array([4,8,16,32])
+Temps   = [1,2,3,4]
+N_warm  = 10**2
+N_meas  = 10**3
 J = -1.0            # Ising lattice coupling constant 
 			        # J > 0: Ferromagnetic 
 			        # J < 0: Antiferromagnetic
@@ -61,64 +65,62 @@ J = -1.0            # Ising lattice coupling constant
 
 # Initialisation for code
 H = 0.0             # Ising lattice field strength 
-k_b = k
-Anzahl_Gitterplätze = L*L
-Inn = np.array([0, 0, 1, -1])
-Jinn = np.array([1, -1, 0, 0])
-grid = rand_init(L)
-mag_T = np.zeros( len(Temps) )
+
+mag_T        = np.zeros( len(Temps) )
 mag_square_T = np.zeros( len(Temps) )
 mag_fourth_T = np.zeros( len(Temps) )
-binder = np.zeros( len(Temps) )
+binder       = np.zeros( len(Temps) )
 
+for size in Sizes:
+    Num  = size**2
+    grid = rand_init(size) 
 
-for ind in range(len(size)):
-    N_warm_up = size[ind]
-    N_mess = size[ind]
-
-    for temperatures in range(len(Temps)):
-        Temp = Temps[temperatures]
-        mag = 0 
-        mag_square = 0 
-        mag_fourth = 0 
+    for temp, j in zip(Temps, range(len(Temps))):
+        #Temp        = Temps[j]
+        mag         = 0 
+        mag_square  = 0 
+        mag_fourth  = 0 
 
         # warm up sweeps
-        for n in range(N_warm_up):
-            grid = sweep(grid, Anzahl_Gitterplätze, Temp)
-        
-        # messungen
-        for n in range(N_mess):
-            grid = sweep(grid, Anzahl_Gitterplätze, Temp)
-            mag = mag + np.sum(grid)/(L*L)
-            mag_square = mag_square + (np.sum(grid)/(L*L))**2 
-            mag_fourth = mag_fourth + (np.sum(grid)/(L*L))**4
+        for n in range(N_warm):
+            print("Warmup: ",round((n+1)/(10**4) * 100, 1),"%", end = "\r")
+            grid = sweep(grid, Num, temp)
+        print('\nwarmup complete ... ')
 
-        mag_T[temperatures] = mag/N_mess
-        mag_square_T[temperatures] = mag_square/N_mess
-        mag_fourth_T[temperatures] = mag_fourth/N_mess
+        # messungen
+        for n in range(N_meas):
+            print("Measurement: ",round((n+1)/(N_meas) * 100, 1),"%", end = "\r")
+            grid        = sweep(grid, Num, temp)
+            mag         = mag + np.sum(grid)/Num
+            mag_square  = mag_square + (np.sum(grid)/Num)**2 
+            mag_fourth  = mag_fourth + (np.sum(grid)/Num)**4
+
+        mag_T[j]        = mag/size
+        mag_square_T[j] = mag_square/size
+        mag_fourth_T[j] = mag_fourth/size
         # Binder Kumulante:
-        binder[temperatures] = 1 - 1/3 * mag_square_T[temperatures]/(mag_square_T[temperatures] * mag_square_T[temperatures])
+        binder[j] = 1 - 1/3 * mag_square_T[j]/(mag_square_T[j] * mag_square_T[j])
 
     pl.plot(mag_T)
-    pl.title("magnetisation over temp; System size = " +str(N_mess)+   "x" + str(N_warm_up))
+    pl.title("magnetisation over temp; System size = " +str(size)+   "x" + str(size))
     pl.xlabel("Temperature")
     pl.ylabel("magnetisation")
     pl.show()
 
     pl.plot(mag_square_T)
-    pl.title("magnetisation squared over temp; System size = " +str(N_mess)+   "x" + str(N_warm_up))
+    pl.title("magnetisation squared over temp; System size = " +str(size)+   "x" + str(size))
     pl.xlabel("Temperature")
     pl.ylabel("magnetisation squared")
     pl.show()
 
     pl.plot(mag_fourth_T)
-    pl.title("magnetisation to the fourth power over temp; System size = " +str(N_mess)+   "x" + str(N_warm_up))
+    pl.title("magnetisation to the fourth power over temp; System size = " +str(size)+   "x" + str(size))
     pl.xlabel("Temperature")
     pl.ylabel("magnetisation^4")
     pl.show()
 
     pl.plot(binder)
-    pl.title("Binder-Kumulante; System size = " +str(N_mess)+   "x" + str(N_warm_up))
+    pl.title("Binder-Kumulante; System size = " +str(size)+   "x" + str(size))
     pl.xlabel("Temperature")
     pl.ylabel("Binder-Kumulante")
     pl.show()
